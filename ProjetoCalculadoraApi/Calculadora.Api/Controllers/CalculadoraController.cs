6ceb0a327764655660ffbea992aab70811a00d8c
Carina Roriz Pimenta <carinaroriz@hotmail.com>
2020-03-18 22:39:20 -0300

    Adicionados testes

--- ProjetoCalculadoraApi/Calculadora.Api/Controllers/CalculadoraController.cs
+++ ProjetoCalculadoraApi/Calculadora.Api/Controllers/CalculadoraController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace CalculadoraApi.Controllers
+namespace Calculadora.Api
 {
     [ApiController]
     [Route("[controller]")]
--- ProjetoCalculadoraApi/Calculadora.Api/Services/CalculadoraService.cs
+++ ProjetoCalculadoraApi/Calculadora.Api/Services/CalculadoraService.cs
@@ -1,22 +1,25 @@
-public class CalculadoraService
+namespace Calculadora.Api
 {
-    public decimal Soma(decimal valor1, decimal valor2)
+    public class CalculadoraService
     {
-        return valor1 + valor2;
-    }
+        public decimal Soma(decimal valor1, decimal valor2)
+        {
+            return valor1 + valor2;
+        }
 
-    public decimal Subtracao(decimal valor1, decimal valor2)
-    {
-        return valor1 - valor2;
-    }
+        public decimal Subtracao(decimal valor1, decimal valor2)
+        {
+            return valor1 - valor2;
+        }
 
-    public decimal Multiplicacao(decimal valor1, decimal valor2)
-    {
-        return valor1 * valor2;
-    }
+        public decimal Multiplicacao(decimal valor1, decimal valor2)
+        {
+            return valor1 * valor2;
+        }
 
-    public decimal Divisao(decimal valor1, decimal valor2)
-    {
-        return valor1 / valor2;
+        public decimal Divisao(decimal valor1, decimal valor2)
+        {
+            return valor1 / valor2;
+        }
     }
 }
